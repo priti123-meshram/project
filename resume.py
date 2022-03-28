af3350cbddf922937fbb4551ec72b878eed9d8d3
priti123-meshram <80310091+priti123-meshram@users.noreply.github.com>
2022-03-28 11:02:53 +0530

    Add files via upload

---
+++ resume.py
@@ -0,0 +1,134 @@
+from tkinter import *
+window=Tk()
+# btn=Button(window, fg='blue')
+# btn.place(x=1000, y=100)
+#----------------------------------------------------------------------------------------------
+lbl=Label(window, text="PRITI MESHRAM", fg='blue', font=("Helvetica", 30))
+lbl.place(x=60, y=50)
+#-------------------------------------------------------------------------------------------------
+# txtfld=Entry(window, text="ahe", bd=5)
+# txtfld.place(x=1000, y=8000)
+lbl=Label(window, text="At-Rasa Dict-Yavtamal", fg='black', font=("Helvetica", 15))
+lbl.place(x=60, y=100)
+#-------------------------------------------------------------------------------------------------
+lbl=Label(window, text="State- Maharashtra", fg='black', font=("Helvetica", 15))
+lbl.place(x=60, y=130)
+#-----------------------------------------------------------------------------------------
+lbl=Label(window, text="mobaile number - 7218801491", fg='black', font=("Helvetica", 13))
+lbl.place(x=59, y=160)
+
+lbl=Label(window, text="Date Of Birth:- 10/11/1999", fg='black', font=("Helvetica", 12))
+lbl.place(x=59, y=190)
+
+lbl=Label(window, text="Email adreass:- pritimeshram20@navgurukul.org", fg='black', font=("Helvetica", 12))
+lbl.place(x=59, y=220)
+
+
+#-----------------------------------------------------------------------------------------------
+lbl=Label(window, text="___________________________________________________________", fg='black', font=("Helvetica", 20))
+lbl.place(x=59, y=250)
+
+lbl=Label(window, text="EDUCATION BACKGROUND", fg='blue', font=("Helvetica", 20))
+lbl.place(x=60, y=280)
+
+lbl=Label(window, text="Education - 10th pass", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=310)
+
+lbl=Label(window, text="School Name / Shvshakti Primary School Kalamb", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=340)
+
+lbl=Label(window, text="Education - 12th pass", fg='black', font=("Helvetica", 14))
+lbl.place(x=560, y=310)
+
+lbl=Label(window, text="College Name / Shvshakti Juniar College Kalamb", fg='black', font=("Helvetica", 14))
+lbl.place(x=560, y=340)
+
+lbl=Label(window, text="Degree - BSW - Bachlor Of Social Work", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=430)
+
+lbl=Label(window, text="College Name / Savitri Jyotirao Social Work", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=460)
+
+lbl=Label(window, text="College Yavtamal", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=490)
+
+lbl=Label(window, text="Flora Institute", fg='black', font=("Helvetica", 14))
+lbl.place(x=560, y=430)
+
+
+
+lbl=Label(window, text="asdrees /  Waghajai Nagar, Katraj,", fg='black', font=("Helvetica", 14))
+lbl.place(x=560, y=460)
+
+lbl=Label(window, text="Satara Highway, Pune", fg='black', font=("Helvetica", 14))
+lbl.place(x=560, y=490)
+
+lbl=Label(window, text="___________________________________________________________", fg='black', font=("Helvetica",20))
+lbl.place(x=59, y=520)
+
+lbl=Label(window, text="SKILL", fg='blue', font=("Helvetica", 20))
+lbl.place(x=60, y=550)
+
+lbl=Label(window, text="MS-CIT - MS-office, Excel word, Power Point, window 7,", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=580)
+
+lbl=Label(window, text="Result _pass", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=610)
+
+lbl=Label(window, text="Python Language - Besic ", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=640)
+
+lbl=Label(window, text="___________________________________________________________", fg='black', font=("Helvetica", 20))
+lbl.place(x=59, y=670)
+
+lbl=Label(window, text="LANGUAGE", fg='blue', font=("Helvetica", 20))
+lbl.place(x=60, y=700)
+
+lbl=Label(window, text="English - Good,", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=730)
+
+lbl=Label(window, text="Hindi - Good,", fg='black', font=("Helvetica", 14))
+lbl.place(x=260, y=730)
+
+lbl=Label(window, text="Marathi - Excellent", fg='black', font=("Helvetica", 14))
+lbl.place(x=450, y=730)
+
+lbl=Label(window, text="___________________________________________________________", fg='black', font=("Helvetica", 20))
+lbl.place(x=59, y=760)
+
+lbl=Label(window, text="PROJECT", fg='blue', font=("Helvetica", 20))
+lbl.place(x=60, y=790)
+
+lbl=Label(window, text="Request, Web-Scrapping   ,", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=820)
+
+
+lbl=Label(window, text="Buity Prodact , Hangman", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=850)
+
+lbl=Label(window, text="KBC , Logine Sine-up", fg='black', font=("Helvetica", 14))
+lbl.place(x=60, y=880)
+
+lbl=Label(window, text="___________________________________________________________", fg='black', font=("Helvetica", 20))
+lbl.place(x=59, y=910)
+
+window.title('RESUME')
+window.geometry("1000x1000+10+10")
+window.mainloop()
+
+
+
+
+
+
+# from tkinter import *
+# window=Tk()
+# window.title('Resume')
+# window.geometry("1000x1000+10+10")
+# window.mainloop()
+
+
+
+
+
+
